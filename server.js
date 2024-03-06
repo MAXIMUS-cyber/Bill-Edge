3c4ccdbeb8d4298787f73bf635aaceec2f538992
Ritabrata Das <153274639+RitabrataDas-INT023@users.noreply.github.com>
2024-03-06 14:26:19 +0530

    only added the server.js removed all other api files as Json Server handles them

---
+++ server.js
@@ -0,0 +1,35 @@
+import Chance from 'chance';
+
+const chance = new Chance();
+
+const baseUrl = 'http://localhost:4000'
+
+const users = [
+    {
+        email: chance.email(),
+        password: chance.string({length:10}),
+        role:['admin']
+    },
+    {
+        email: chance.email(),
+        password: chance.string({length:10}),
+        role:['user']
+    },
+    {
+        email: chance.email(),
+        password: chance.string({length:10}),
+        role:['user']
+    }
+]
+
+console.log("RANDOM USERS",users)
+
+for(let user of users){
+    await fetch(`${baseUrl}/register`,{
+        method:'POST',
+        body:JSON.stringify(user),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+}
